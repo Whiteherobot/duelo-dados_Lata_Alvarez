e10bbf48a002af2111ecd14dc37a31aa177cc1a7
Michi <mlata6235@gmail.com>
2025-04-02 07:18:40 -0500

    Duelo de Dados Completado

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function lanzarDado() {
             }
         }
 
-        if (turn >= 18) {
+        if (turn >= 6) {
             juegoTerminado = true;
             determinarGanador();
             agregarGanadorAlHistorial();
